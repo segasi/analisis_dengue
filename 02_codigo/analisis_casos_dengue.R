9f369796a1e65ea5eaad2b0fd287bd9cf01fe3bc
Sebastian Garrido <segasi@gmail.com>
2019-09-07 13:13:00 -0500

    Importar datos

--- 02_codigo/analisis_casos_dengue.R
+++ 02_codigo/analisis_casos_dengue.R
@@ -1,2 +1,6 @@
 ### Cargar paquetes, definir setup general y tema de gráficas ----
 source("02_codigo/paquetes_setup_tema.R")
+
+### Importar datos ----
+bd <- 
+  read_excel("01_datos/casos_dengue.xlsx")
